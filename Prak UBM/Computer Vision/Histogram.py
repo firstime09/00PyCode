93f3e3db46c035f6428518aea8edad92ce2aabcf
FFTampinongkol <fftampinongkol09@gmail.com>
2021-03-08 14:00:00 +0800

    UP New

--- Prak UBM/Computer Vision/Histogram.py
+++ Prak UBM/Computer Vision/Histogram.py
@@ -17,3 +17,32 @@ plt.show()
 """cv2.imshow('Name',img_1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()"""
+
+## Materi Kecerdasan Buatan GA - Praktek
+import random, datetime
+
+def generateParent(length):
+    genes = list("")
+    for i in range(0,length):
+        geneIndex = random.randint(0, len(geneset) -1);
+        genes.append(geneset[geneIndex])
+    return(''.join(genes))
+
+def getFitness(candidate, target):
+   fitness = 0
+   for i in range(0, len(candidate)):
+       if target[i] == candidate[i]:
+           fitness += 1
+   return(fitness)
+
+def mutate(parent):
+   geneIndex = random.randint(0, len(geneset) -1);
+   index = random.randint(0, len(parent) - 1)
+   genes = list(parent)
+   genes[index] = geneset[geneIndex]
+   return(''.join(genes))
+
+def display(candidate, startTime):
+    timeDiff = datetime.datetime.now() - startTime
+    fitness = getFitness(candidate, target)
+    print("%s\t%i\t%s" % (candidate, fitness, str(timeDiff)))
